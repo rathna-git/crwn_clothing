1bfacd7782c94fd6044adf3247287ec82b7da9e5
Rathna Ganjigunta <rathna.ganjigunta@gmail.com>
2020-03-04 17:35:51 -0800

    storing users in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:217544544631:web:49ca5b6c0c0b7a01029172"
   };
 
+export const createUserProfileDocument = async(userAuth,additionalData) => {
+  if(!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    }catch(error){
+      console.log('Error creating User', error.message);
+    }
+  }
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
